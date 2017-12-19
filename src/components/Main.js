71d61757e47ee5332ea4be7d445fb59873d9e691
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-19 13:39:04 -0200

    Add route components to Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,16 @@ import { Switch, Route } from 'react-router-dom';
 // Routes components
 import OrderHistory from './OrderHistory';
 import User from './User';
+import AddressList from './AddressList';
+import Wishlist from './Wishlist.js';
 
 function Main() {
   return (
     <Switch>
         <Route exact path="/" component={User} />
-        <Route exact path="/order-history" component={OrderHistory} />
+        <Route exact path="/order-history/:userId" component={OrderHistory} />
+        <Route exact path="/address-list/:userId" component={AddressList} />
+        <Route exact path="/wishlist/:userId" component={Wishlist} />
     </Switch>
   );
 }
