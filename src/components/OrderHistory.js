c1bfe83ddb25e714e7f167f1df0ad9aed6a0772a
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-20 20:12:26 -0200

    Implement OrderHistory component

--- src/components/OrderHistory.js
+++ src/components/OrderHistory.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react'
+import UserDataHeader from './UserDataHeader'
+import BackToMyAccount from './BackToMyAccount'
 
-const OrderHistory = props => {
-    return <div>Order history</div>
+import loadUserData from '../utils/load-user-data'
+import loadProducts from '../utils/load-products'
+
+class OrderHistory extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            productList: [],
+            user: {}
+        }
+
+        this.handleProductList = this.handleProductList.bind(this)
+        this.handleUserData = this.handleUserData.bind(this)
+        
+    }
+    
+    componentDidMount() {
+        loadUserData(this.handleUserData)
+            .then(userId => loadProducts(userId, 'orderHistory', this.handleProductList))
+    }
+
+    render() {
+        return (
+            <div>
+                <UserDataHeader user={this.state.user} />
+                <div>{this.renderProductList()}</div>
+                <BackToMyAccount />
+            </div>
+        )
+    }
+
+    handleUserData(data) {
+        this.setState({ user: data })
+        return data.userId
+    }
+
+    renderProductList() {
+        console.log(this.state.productList)
+        return this.state.productList.length === 0 ?
+        <div>Loading...</div> :
+        this.state.productList.map((product, index) =>
+            <div key={index}>{product.name}</div>
+        )
+    }
+
+    handleProductList(data) {
+        this.setState({ productList: data })
+    }
 }
 
-export default OrderHistory;
+export default OrderHistory
