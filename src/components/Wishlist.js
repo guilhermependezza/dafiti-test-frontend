ef919e84dc5ec904501faaccdbff857026064301
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-20 15:40:22 -0200

    Initial implementation of user's wishlist

---
+++ src/components/Wishlist.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import UserDataHeader from './UserDataHeader';
+
+import loadUserData from '../utils/load-user-data'
+import loadProducts from '../utils/load-products'
+
+class Wishlist extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            productList: [],
+            user: {}
+        }
+
+        this.handleProductList = this.handleProductList.bind(this)
+        this.handleUserData = this.handleUserData.bind(this)
+        
+    }
+    
+    componentDidMount() {
+        loadUserData(this.handleUserData)
+            .then(userId => loadProducts(userId, 'wishlist', this.handleProductList))
+    }
+
+    render() {
+        return (
+            <div>
+                <UserDataHeader user={this.state.user} />
+                <div>{this.renderProductList()}</div>
+            </div>
+        )
+    }
+
+    handleUserData(data) {
+        this.setState({ user: data })
+        return data.userId
+    }
+
+    renderProductList() {
+        console.log(this.state.productList)
+        return this.state.productList.length === 0 ?
+        <div>Loading...</div> :
+        this.state.productList.map((product, index) =>
+            <div key={index}>{product.name}</div>
+        )
+    }
+
+    handleProductList(data) {
+        this.setState({ productList: data })
+    }
+}
+
+export default Wishlist
