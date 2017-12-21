f83a4e5fd0cef8f99553389166e27239e3d7c26d
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-21 15:23:02 -0200

    Add some markup to BackToMyAccount component

--- src/components/BackToMyAccount.js
+++ src/components/BackToMyAccount.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const BackToMyAccount = () => {
-    return <Link to='/'>Back to my account</Link>
+    return <div class="actions"><Link className="actions__link" to='/'>Back to my account</Link></div>
 }
 
 export default BackToMyAccount
