54baaff389804b319918750358739709c3f8e520
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-22 16:50:31 -0200

    Add Header

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -39,32 +39,35 @@ class User extends Component {
 
     renderEditForm() {
         return (
-            <section className="user-data">
-                <div className="edit-field">
-                    <label className="edit-field__label">User id:</label>
-                    <span className="display-field__value">{this.state.userData.userId}</span>
-                </div>
-                <div className="edit-field">
-                    <label className="edit-field__label">Name:</label>
-                    <input type="text" className="edit-field__value" value={this.state.userData.name} onChange={this.onChange.bind(this, 'name')} />
-                </div>
-                <div className="edit-field">
-                    <label className="edit-field__label">Age:</label>
-                    <input type="text" className="edit-field__value" value={this.state.userData.age} onChange={this.onChange.bind(this, 'age')} />
-                </div>
-                <div className="edit-field">
-                    <label className="edit-field__label">Phone:</label>
-                    <input type="text" className="edit-field__value" value={this.state.userData.phone} onChange={this.onChange.bind(this, 'phone')} />
-                </div>
-                <div className="edit-field">
-                    <label className="edit-field__label">e-mail:</label>
-                    <input type="text" className="edit-field__value" value={this.state.userData.email} onChange={this.onChange.bind(this, 'email')} />
-                </div>
-                <div className="actions">
-                    <button className="actions__link" href='#' onClick={this.saveButton} >Save user info</button>
-                    <button className="actions__link" href='#' onClick={this.cancelButton} >Cancel</button>
-                </div>
-            </section>
+            <div>
+                <h1>My account</h1>
+                <section className="user-data">
+                    <div className="edit-field">
+                        <label className="edit-field__label">User id:</label>
+                        <span className="display-field__value">{this.state.userData.userId}</span>
+                    </div>
+                    <div className="edit-field">
+                        <label className="edit-field__label">Name:</label>
+                        <input type="text" className="edit-field__value" value={this.state.userData.name} onChange={this.onChange.bind(this, 'name')} />
+                    </div>
+                    <div className="edit-field">
+                        <label className="edit-field__label">Age:</label>
+                        <input type="text" className="edit-field__value" value={this.state.userData.age} onChange={this.onChange.bind(this, 'age')} />
+                    </div>
+                    <div className="edit-field">
+                        <label className="edit-field__label">Phone:</label>
+                        <input type="text" className="edit-field__value" value={this.state.userData.phone} onChange={this.onChange.bind(this, 'phone')} />
+                    </div>
+                    <div className="edit-field">
+                        <label className="edit-field__label">e-mail:</label>
+                        <input type="text" className="edit-field__value" value={this.state.userData.email} onChange={this.onChange.bind(this, 'email')} />
+                    </div>
+                    <div className="actions">
+                        <button className="actions__link" href='#' onClick={this.saveButton} >Save user info</button>
+                        <button className="actions__link" href='#' onClick={this.cancelButton} >Cancel</button>
+                    </div>
+                </section>
+            </div>
         )
     }
 
@@ -97,34 +100,37 @@ class User extends Component {
     renderDisplayForm() {
         const { userId, name, age, phone, email } = this.state.userData
         return (
-            <section className="user-data">
-                <div className="display-field">
-                    <label className="display-field__label">User id:</label>
-                    <span className="display-field__value">{userId}</span>
-                </div>
-                <div className="display-field">
-                    <label className="display-field__label">Name:</label>
-                    <span className="display-field__value">{name}</span>
-                </div>
-                <div className="display-field">
-                    <label className="display-field__label">Age:</label>
-                    <span className="display-field__value">{age}</span>
-                </div>
-                <div className="display-field">
-                    <label className="display-field__label">Phone:</label>
-                    <span className="display-field__value">{phone}</span>
-                </div>
-                <div className="display-field">
-                    <label className="display-field__label">e-mail:</label>
-                    <span className="display-field__value">{email}</span>
-                </div>
-                <button onClick={this.editButton} >Edit user info</button>
-                <div className="actions">
-                    <Link className="actions__link" to={`/address-list/${userId}`}>View address list</Link>
-                    <Link className="actions__link" to={`/wishlist/${userId}`}>Wishlist</Link>
-                    <Link className="actions__link" to={`/order-history/${userId}`}>Order history</Link>
-                </div>
-            </section>
+            <div>
+                <h1>My account</h1>
+                <section className="user-data">
+                    <div className="display-field">
+                        <label className="display-field__label">User id:</label>
+                        <span className="display-field__value">{userId}</span>
+                    </div>
+                    <div className="display-field">
+                        <label className="display-field__label">Name:</label>
+                        <span className="display-field__value">{name}</span>
+                    </div>
+                    <div className="display-field">
+                        <label className="display-field__label">Age:</label>
+                        <span className="display-field__value">{age}</span>
+                    </div>
+                    <div className="display-field">
+                        <label className="display-field__label">Phone:</label>
+                        <span className="display-field__value">{phone}</span>
+                    </div>
+                    <div className="display-field">
+                        <label className="display-field__label">e-mail:</label>
+                        <span className="display-field__value">{email}</span>
+                    </div>
+                    <button onClick={this.editButton} >Edit user info</button>
+                    <div className="actions">
+                        <Link className="actions__link" to={`/address-list/${userId}`}>View address list</Link>
+                        <Link className="actions__link" to={`/wishlist/${userId}`}>Wishlist</Link>
+                        <Link className="actions__link" to={`/order-history/${userId}`}>Order history</Link>
+                    </div>
+                </section>
+            </div>
         )
     }
 }
