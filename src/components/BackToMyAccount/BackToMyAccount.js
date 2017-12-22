8c425a2f9ce63c4b757603dda56f7244f0555047
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-22 17:17:59 -0200

    Fix attribute name

--- src/components/BackToMyAccount/BackToMyAccount.js
+++ src/components/BackToMyAccount/BackToMyAccount.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const BackToMyAccount = () => {
-    return <div class="actions"><Link className="actions__link" to='/'>Back to my account</Link></div>
+    return <div className="actions"><Link className="actions__link" to='/'>Back to my account</Link></div>
 }
 
 export default BackToMyAccount
